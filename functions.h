6fbf7d71b778883fcbb18ab0f314eeb831fd5141
oisin <oisinbrady@protonmail.com>
2018-12-08 18:31:22 +0000

    reconfigured menuLoop and produced some printing statements for teachingTimes 2D-array. N.B. option 3 (quit) on menu still doesn't work as intended.

--- functions.h
+++ functions.h
@@ -3,19 +3,18 @@ typedef int bool;
 #define false 0
 int numberOfModules;
 int numberOfSchemes;
+char clashArray[100][8];
 typedef struct module{
     char moduleID[7];
     int semester;
     char lectureAmountAndHr[4];
     char pracAmountAndHr[4];
 } Module;
-
 typedef struct coreModule {
     char moduleID[7];
     int semester;
     struct coreModule *nextCoreModule;
 } CoreModule;
-
 typedef struct scheme{
     char schemeCode[5];
     int yearOfStudy;
@@ -23,16 +22,13 @@ typedef struct scheme{
     int numberOfCoreModules;
     CoreModule *coreModule; //pointer to the first core module in a linked list (header)
 } Scheme;
-char clashArray[100][8];
-
 char *getFolder(void);
 Module * readModules(char *file);
 Scheme * readSchemes(char *file, Module *modulesList);
 int ** readTimes(char *file); //7 days a week w/ a max of 9 hours teaching/day
-
-void buildTimetable(Module *modulesList, Scheme * schemesList, int ** teachingTimeSlots);
+void printMenu(void);
+void menuLoop(Module *moduleList, Scheme *schemesList, int **teachingTimes);
 void moduleInfo(Module * modulesList, int ** teachingTimes, Scheme * schemesList);
 void initialiseClashArray(Scheme scheme, int semester, const char *moduleID);
 void addClash(Scheme scheme, int semester, const char *moduleID);
-void printMenu(void);
-void menuLoop(Module *moduleList, Scheme *schemesList, int **teachingTimes);
+void buildTimetable(Module *modulesList, Scheme * schemesList, int ** teachingTimeSlots);
